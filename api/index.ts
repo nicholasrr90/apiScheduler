f7e15501e1553ba53e203aadac2745383c64a1ea
nicholasrr90 <nicholasrodrigues.eu@gmail.com>
2024-07-22 20:06:35 -0300

    teste git

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import cors from "cors";
 import { teacherRoutes } from "../src/routes/teacher.routes";
 import { studentRoutes } from "../src/routes/student.routes";
@@ -10,16 +10,24 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Definir rotas
-app.get("/", (req, res) => {
-  res.send({ message: "DEU CERTO CARAIO!" });
+// Define routes
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "teste github" });
 });
 
-app.use("/", teacherRoutes);
-app.use("/", studentRoutes);
-app.use("/", lessonRoutes);
+app.use("/teacher", teacherRoutes);
+app.use("/student", studentRoutes);
+app.use("/lesson", lessonRoutes);
 
-// Iniciar o servidor
+// Error handling middleware
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: 'Something went wrong!' });
+};
+
+app.use(errorHandler);
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
